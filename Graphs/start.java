efa6061b73005b60ae52f993879d0d90843dd33f
Wale1412 <yonasw93@gmail.com>
2019-03-17 15:26:44 -0700

    Update start.java

--- Graphs/start.java
+++ Graphs/start.java
@@ -13,22 +13,16 @@ public class start{
         addEdge(3,1);
         addEdge(3,2);
         print();
-
-
     }
 
     public static void addEdge(int i, int n){
-
         graph.adjacencyList[i].add(n);
-
     }
     public static void print(){
-
         for(int i = 1; i<graph.adjacencyList.length-1; i++){
             System.out.print(i+" ");
             System.out.println(graph.adjacencyList[i]);
         }
-
     }
 
     public static void breadthFirstTraversal(){}
