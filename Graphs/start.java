99efce81a6544787b5d4158fe91e4815815ba124
Wale1412 <yonasw93@gmail.com>
2019-03-17 16:34:09 -0700

    Breadth First Search

--- Graphs/start.java
+++ Graphs/start.java
@@ -1,5 +1,8 @@
 package Graphs;
 
+import javax.annotation.processing.SupportedSourceVersion;
+import java.util.Iterator;
+
 public class start{
 
     private static Graph graph;
@@ -13,19 +16,40 @@ public class start{
         addEdge(3,1);
         addEdge(3,2);
         print();
+        breadthFirstTraversal();
     }
 
     public static void addEdge(int i, int n){
         graph.adjacencyList[i].add(n);
     }
     public static void print(){
+        System.out.println("i " + "adjacentNodes");
+        System.out.println("- ------------");
         for(int i = 1; i<graph.adjacencyList.length-1; i++){
             System.out.print(i+" ");
             System.out.println(graph.adjacencyList[i]);
         }
     }
 
-    public static void breadthFirstTraversal(){}
+    public static void breadthFirstTraversal(){
+
+        int [] visited = new int[graph.v];
+        System.out.println("Breadth First Traversal");
+
+
+        for(int i = 1; i < visited.length-1; i++){
+            if(visited[i] == 0){
+                System.out.print(i+ " ");
+                visited[i] = 1;
+                for (Iterator j = graph.adjacencyList[i].iterator(); j.hasNext();){
+                    int k = (int) j.next();
+                    System.out.println(k);
+                    visited[k] = 1;
+                }
+            }
+            System.out.println();
+        }
+    }
     public static void depthFirstTraversal(){}
 
 
