29e5d8b4385979ca01568ab2e1f85cc95832680a
Wale1412 <yonasw93@gmail.com>
2019-03-17 20:50:07 -0700

    Done

--- Graphs/Graph.java
+++ Graphs/Graph.java
@@ -1,5 +1,6 @@
 package Graphs;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class Graph {
@@ -14,6 +15,31 @@ public class Graph {
         for(int i=0; i<v; i++){
             adjacencyList[i] = new LinkedList<>();
         }
+    }
+    public void breadthFirstTraversal(int s){
+
+        int [] visited = new int[v];
+        System.out.println("Breadth First Traversal");
+        LinkedList<Integer> q = new LinkedList<Integer>();
+        visited[s] = 1;
+        q.add(s);
+        while (q.size() != 0 ){
+            s = q.poll();
+            Iterator<Integer> itr = adjacencyList[s].listIterator();
+            System.out.print(s + " ");
+            while (itr.hasNext()){
+
+                int k = itr.next();
+                if(visited[k] == 0){
+                    visited[k] = 1;
+                    q.add(k);
+                }
+            }
+        }
+    }
+
+    public void addEdge(int s, int n){
+        adjacencyList[s].add(n);
     }
 
 
--- Graphs/start.java
+++ Graphs/start.java
@@ -2,26 +2,25 @@ package Graphs;
 
 import javax.annotation.processing.SupportedSourceVersion;
 import java.util.Iterator;
+import java.util.LinkedList;
 
 public class start{
 
     private static Graph graph;
     public static void main(String [] args){
 
-        graph = new Graph(5);
-        addEdge(1,2);
-        addEdge(1,3);
-        addEdge(2,1);
-        addEdge(2,3);
-        addEdge(3,1);
-        addEdge(3,2);
+        graph = new Graph(4);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 0);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 3);
         print();
-        breadthFirstTraversal();
+        graph.breadthFirstTraversal(2);
     }
 
-    public static void addEdge(int i, int n){
-        graph.adjacencyList[i].add(n);
-    }
+
     public static void print(){
         System.out.println("i " + "adjacentNodes");
         System.out.println("- ------------");
@@ -31,25 +30,7 @@ public class start{
         }
     }
 
-    public static void breadthFirstTraversal(){
 
-        int [] visited = new int[graph.v];
-        System.out.println("Breadth First Traversal");
-
-
-        for(int i = 1; i < visited.length-1; i++){
-            if(visited[i] == 0){
-                System.out.print(i+ " ");
-                visited[i] = 1;
-                for (Iterator j = graph.adjacencyList[i].iterator(); j.hasNext();){
-                    int k = (int) j.next();
-                    System.out.println(k);
-                    visited[k] = 1;
-                }
-            }
-            System.out.println();
-        }
-    }
     public static void depthFirstTraversal(){}
 
 
